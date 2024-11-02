0bea7364cb540754ad3bd0f1e2ee7134e7110fc9
katherinehambley <superdesigngirl@outlook.com>
2024-11-02 15:54:51 -0500

    Added CollectionView to HeadlinesPage

--- Pages/HeadlinesPage.xaml.cs
+++ Pages/HeadlinesPage.xaml.cs
@@ -1,9 +1,25 @@
-﻿namespace MauiNewsApp2.Pages;
+﻿using MauiNewsApp2.ViewModels;
+
+namespace MauiNewsApp2.Pages;
 
 public partial class HeadlinesPage : ContentPage
 {
-	public HeadlinesPage()
+    readonly HeadlinesViewModel viewModel;
+
+    public HeadlinesPage(HeadlinesViewModel viewModel)
 	{
-		InitializeComponent();
-	}
+        this.viewModel = viewModel;
+        InitializeComponent();
+        Task.Run(async () => await Initialize(GetScopeFromRoute()));
+    }
+    private async Task Initialize(string scope)
+    {
+        BindingContext = viewModel;
+        await viewModel.Initialize(scope);
+    }
+    private string GetScopeFromRoute()
+    {
+        var route = Shell.Current.CurrentState.Location.OriginalString.Split("/").LastOrDefault();
+        return route;
+    }
 }
