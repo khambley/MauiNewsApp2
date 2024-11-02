f39432b588287fb96c83a41de9f0c02db1ff20d9
katherinehambley <superdesigngirl@outlook.com>
2024-11-02 14:48:12 -0500

    Added models

---
+++ Models/NewsResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace MauiNewsApp2.Models
+{
+	public class NewsResult
+	{
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("totalResults")]
+        public int TotalResults { get; set; }
+
+        [JsonPropertyName("articles")]
+        public List<Article> Articles { get; set; }
+    }
+}
+
---
+++ Models/Source.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace MauiNewsApp2.Models
+{
+	public class Source
+	{
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
+
